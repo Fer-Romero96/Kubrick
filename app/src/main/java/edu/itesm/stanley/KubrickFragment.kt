50d7baec5a0b1fc585f0589887a430ef313d0ad9
Fer-Romero96 <A01335834@itesm.mx>
2021-03-11 13:10:40 -0600

    Movies completo

--- app/src/main/java/edu/itesm/stanley/KubrickFragment.kt
+++ app/src/main/java/edu/itesm/stanley/KubrickFragment.kt
@@ -1,10 +1,13 @@
 package edu.itesm.stanley
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_kubrick.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -35,6 +38,16 @@ class KubrickFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        movies.setOnClickListener {
+            Log.i("Kubrik","Nav Component")
+            view?.findNavController()?.navigate(R.id.action_kubrickFragment_to_moviesFragment)
+            //Navigation.createNavigateOnClickListener( R.id.action_kubrickFragment_to_moviesFragment)
+        }
+
+        actors.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_kubrickFragment_to_actorskFragment)
+        //Navigation.createNavigateOnClickListener(R.id.action_kubrickFragment_to_actorskFragment, null)
+        }
     }
 
 
--- app/src/main/java/edu/itesm/stanley/MoviesFragment.kt
+++ app/src/main/java/edu/itesm/stanley/MoviesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_movies.*
 
@@ -16,7 +17,6 @@ import kotlinx.android.synthetic.main.fragment_movies.*
  * create an instance of this fragment.
  */
 class MoviesFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
